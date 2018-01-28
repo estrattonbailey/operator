ab1beb5bdfcf685b76d265ab1f04b2aef08cefc6
estrattonbailey <ericstrattonbailey@gmail.com>
2018-01-28 17:08:42 -0500

    .

---
+++ index.js
@@ -0,0 +1,145 @@
+function getParts (url) {
+  const parts = url.split('/')
+  return parts.slice(parts[0] !== '' ? 0 : 1)
+}
+
+function clean (href) {
+  return href.replace(window.location.origin, '')
+}
+
+function getRoute (path, routes) {
+  const urls = getParts(path)
+  const params = {}
+  outer: for (let route of routes) {
+    if (urls.length === route.parts.length) {
+      inner: for (let i = 0; i < route.parts.length; i++) {
+        if (route.parts[i][0] === ':') {
+          params[route.parts[i].slice(1)] = urls[i]
+          continue inner
+        } else if (route.parts[i] === urls[i]) {
+          continue inner
+        } else if (route.parts[i] === '*') {
+          break
+        }
+        continue outer
+      }
+      route.params = params
+      return route
+    } else if (route.parts[0] == '*') {
+      route.params = params
+      return route
+    }
+  }
+}
+
+export default function app (defs, selector) {
+  root = document.querySelector(selector)
+
+  let queue
+  const routes = []
+  const before = []
+  const during = []
+  const after = []
+
+  while (defs.length) {
+    const r = defs.shift()
+    if (typeof r === 'function') {
+      during.push(r)
+      continue
+    }
+    routes.push({
+      path: r.pop ? r[0] : r,
+      parts: getParts(r.pop ? r[0] : r),
+      handler: r.pop ? r[1] || null : null
+    })
+  }
+
+  const initialRoute = getRoute(clean(window.location.href), routes)
+
+  const state = {
+    path: clean(window.location.href),
+    title: document.title,
+    params: initialRoute ? initialRoute.params : {}
+  }
+
+  function go (path, route) {
+    queue = () => {
+      fetch(path, { credentials: 'include' }).then(res => res.text()).then(r => {
+        const html = new window.DOMParser().parseFromString(r, 'text/html')
+        state.title = html.title
+        Promise.all(
+          during.reduce((fns, fn) => {
+            fns.push(fn(state))
+            return fns
+          }, [route.handler ? route.handler(state) : true])
+        ).then(() => {
+          requestAnimationFrame(() => {
+            root.innerHTML = html.querySelector(selector).innerHTML
+            for (let fn of after) fn(state)
+          })
+        })
+      })
+    }
+
+    state.path = path
+    state.params = route.params
+    for (let fn of before) fn(path)
+
+    queue()
+  }
+
+  document.body.addEventListener('click', e => {
+    if (e.ctrlKey || e.metaKey || e.altKey || e.shiftKey || e.defaultPrevented) return
+
+    let a = e.target
+
+    while (a && !(a.href && a.nodeName === 'A')) {
+      a = a.parentNode
+    }
+
+    if (
+      !a ||
+      window.location.origin !== a.origin ||
+      a.hasAttribute('download') ||
+      a.target === '_blank' ||
+      /mailto|tel/.test(a.href)
+    ) return
+
+    const path = clean(a.href)
+    const route = getRoute(path, routes)
+    if (!route) return
+    e.preventDefault()
+    go(path, route)
+    return false
+  })
+
+  window.addEventListener('popstate', e => {
+    const path = clean(e.target.location.href)
+    const route = getRoute(path, routes)
+    if (!route) return
+    e.preventDefault()
+    go(path, route)
+    return false
+  })
+
+  return {
+    get state () {
+      return state
+    },
+    push (href) {
+      queue = null
+      const path = clean(href)
+      const route = getRoute(path, routes)
+      if (!route) return
+      go(path, route)
+    },
+    before (fn) {
+      before.push(fn)
+      return () => before.slice(before.indexOf(fn), 1)
+    },
+    after (fn) {
+      after.push(fn)
+      return () => after.slice(after.indexOf(fn), 1)
+    }
+  }
+}
